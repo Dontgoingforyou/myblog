523cec3b2a73306a77df443b3ec789ad2edfb165
Dontgoingforyou <mishael000@yandex.ru>
2024-11-09 20:45:53 +0300

    created a tagging system, generated recommended posts, created a site map, an RSS news feed, transferred the project to PostgreSQL, implemented the PostgreSQL search engine

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostListView, post_detail, post_share, post_comment
+from blog.views import post_list, post_detail, post_share, post_comment, post_search
+from blog.feeds import LatestPostFeed
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    path('', post_list, name='post_list'),
+    path('tag/<str:tag_slug>/', post_list, name='post_list_by_tag'),
     path('<int:day>/<int:month>/<int:year>/<slug:post>/', post_detail, name='post_detail'),
     path('<int:post_id>/share/',post_share, name='post_share'),
     path('<int:post_id>/comment/', post_comment, name='post_comment'),
+    path('feed/', LatestPostFeed(), name='post_feed'),
+    path('search/', post_search, name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,33 +1,34 @@
+from django.contrib.postgres.search import TrigramSimilarity, TrigramWordSimilarity
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView
-from django.http import Http404
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
+from taggit.models import Tag
+from django.db.models import Func, F, Q
 
-from blog.forms import EmailPostForm, CommentForm
+from blog.forms import EmailPostForm, CommentForm, SearchForm
 from blog.models import Post
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
 
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -39,12 +40,19 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__year=year,
     )
+    # список активных комментариев к посту
     comments = post.comments.filter(active=True)
+    # форма для комментариев пользователя
     form = CommentForm()
+    # список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post, 'comments': comments, 'form': form}
+        {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts}
     )
 
 
@@ -90,4 +98,46 @@ def post_comment(request, post_id):
         request,
         'blog/post/comment.html',
         {'post': post, 'form': form, 'comment': comment}
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    # if 'query' in request.GET:
+    #     form = SearchForm(request.GET)
+    #     if form.is_valid():
+    #         query = form.cleaned_data['query']
+    #         results = (
+    #             Post.published.annotate(
+    #                 title_similarity=TrigramSimilarity(Func(F('title'), function='unaccent'), query),
+    #                 body_similarity=TrigramSimilarity(Func(F('body'), function='unaccent'), query),
+    #             )
+    #             .filter(
+    #                 Q(title_similarity__gt=0.1) | Q(body_similarity__gt=0.1)
+    #             )
+    #             .order_by('-title_similarity', '-body_similarity')
+    #         )
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query),
+                title_similarity=TrigramSimilarity('title', query),
+                body_similarity=TrigramSimilarity('body', query),
+            ).filter(
+                Q(search=search_query) | Q(title_similarity__gt=0.1) | Q(body_similarity__gt=0.1)
+            ).order_by('-rank', '-title_similarity', '-body_similarity')
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {'form': form, 'query': query, 'results': results}
     )
